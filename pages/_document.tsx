ccb2a88eee3fe937632e368d382f9a13c1ccc8b0
mateusanjost <mateusanjosmgb@gmail.com>
2021-12-22 12:16:54 -0300

    basic config for nextjs

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Web site created using create-react-app" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ pages/login/index.tsx
@@ -0,0 +1,5 @@
+function login(){
+    return <>teste</>
+}
+
+export default login
