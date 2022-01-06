a3047bb25161c1d8a2958deb24b7214fd6e446f9
mateusanjost <mateusanjosmgb@gmail.com>
2022-01-05 23:32:08 -0300

    refact componetization in Form.tsx

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -32,18 +32,161 @@ function Form({ users }: usersType): JSX.Element {
 
   useEffect(() => setUsersDB(Object.values(users)), [users]);
 
+  const onLoginValidation = useCallback(() => {
+    if (!email && !password)
+      return setErrorLogin("Email and password is required");
+    if (!email) return setErrorLogin("Email is required");
+    if (!password) return setErrorLogin("Password is required");
+    setContTryLogin(contTryLogin + 1);
+  }, [email, password, contTryLogin]);
 
-  const onLoginValidation = useCallback(
-    () => {
-      if(!email && !password) return setErrorLogin("Email and password is required");
-      if(!email) return setErrorLogin("Email is required");
-      if(!password) return setErrorLogin("Password is required");
-      setContTryLogin(contTryLogin + 1);
-    },
-    [users.email, email, password, contTryLogin],
-  )
+  const handleShowClick = useCallback(
+    () => setShowPassword(!showPassword),
+    [showPassword]
+  );
+  const getHeading = () => (
+    <>
+      {errorLogin}
+      <Avatar bg="teal.500" />
+      <Heading color="teal.400" fontSize={"1rem"}>
+        Olá {isLogged ? `, ${email}` : null}
+      </Heading>
+    </>
+  );
 
-  const handleShowClick = useCallback(() => setShowPassword(!showPassword), [showPassword]);
+  const getFormBox = () => (
+    <>
+      <Box minW={{ base: "90%", md: "468px" }}>
+        <Stack
+          spacing={4}
+          p="1rem"
+          backgroundColor="whiteAlpha.900"
+          boxShadow="md"
+        >
+          <FormControl>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                tabIndex={-1}
+                children={<CFaUserAlt color="gray.300" />}
+                htmlFor="email"
+              />
+              <Input
+                type="email"
+                placeholder="E-mail"
+                tabIndex={1}
+                title="E-mail"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </InputGroup>
+          </FormControl>
+          <FormControl>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                color="gray.300"
+                children={<CFaLock color="gray.300" />}
+                tabIndex={-1}
+                htmlFor="password"
+              />
+              <Input
+                type={showPassword ? "text" : "password"}
+                placeholder="Senha"
+                tabIndex={2}
+                title="Senha"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width="4.5rem">
+                <Button
+                  h="1.75rem"
+                  size="sm"
+                  tabIndex={3}
+                  onClick={handleShowClick}
+                  title="Exibir a senha"
+                >
+                  {showPassword ? "Ocultar" : "Exibir"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <FormHelperText textAlign="right">
+              <Link tabIndex={4} title="Esqueceu a sua senha?">
+                Esqueceu a senha?
+              </Link>
+            </FormHelperText>
+          </FormControl>
+          <Button
+            borderRadius={0}
+            type="submit"
+            variant="solid"
+            colorScheme="teal"
+            width="full"
+            tabIndex={5}
+            onClick={() => onLoginValidation()}
+            backgroundColor={contTryLogin > 3 ? "red.500" : "teal.500"}
+            isDisabled={contTryLogin > 3}
+          >
+            {contTryLogin > 3 ? `Tentativas excedidas` : "Entrar"}
+          </Button>
+        </Stack>
+      </Box>
+    </>
+  );
+
+  const getCountTryBox = () => (
+    <>
+      <Box>
+        <Button
+          h="1.75rem"
+          size="sm"
+          tabIndex={1}
+          onClick={() => setContTryLogin(0)}
+          title="Resetar contador de tentativas"
+          width="2.5rem"
+          padding="0.5rem"
+          fontSize="0.5rem"
+          marginLeft={2}
+          backgroundColor={"teal.500"}
+          name={"Resetar contador de tentativas"}
+        >
+          X
+        </Button>{" "}
+        {`Contador de tentativas: ${contTryLogin}`}
+      </Box>
+    </>
+  );
+
+  const getUnSignButton = () => (
+    <Button
+      h="1.75rem"
+      size="sm"
+      tabIndex={1}
+      onClick={() => setIsLogged(false)}
+      title="desconectar-se"
+      width="4.5rem"
+      name="logout"
+    >
+      Sair
+    </Button>
+  );
+
+  const getComplementBox = () => (
+    <>
+      Não possui uma conta?
+      <Link
+        color="teal.500"
+        paddingLeft="10px"
+        href="#"
+        tabIndex={6}
+        title="cadastrar-se"
+      >
+        Cadastrar-se
+      </Link>
+      {getCountTryBox()}
+      {ListUsers(usersDB)}
+    </>
+  );
 
   return (
     <Flex
@@ -61,142 +204,10 @@ function Form({ users }: usersType): JSX.Element {
         justifyContent="center"
         alignItems="center"
       >
-        <Avatar bg="teal.500" />
-        <Heading color="teal.400" fontSize={"1rem"}>
-          Olá {isLogged ? `, ${email}` : null}
-        </Heading>
-
-
-        <Heading fontSize={"1em"} color={"red"}>
-              {errorLogin}
-            </Heading>
-
-
-        <Box minW={{ base: "90%", md: "468px" }}>
-          {!isLogged && (
-            <Stack
-              spacing={4}
-              p="1rem"
-              backgroundColor="whiteAlpha.900"
-              boxShadow="md"
-            >
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    tabIndex={-1}
-                    children={<CFaUserAlt color="gray.300" />}
-                    htmlFor="email"
-                  />
-                  <Input
-                    type="email"
-                    placeholder="E-mail"
-                    tabIndex={1}
-                    title="E-mail"
-                    name="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    color="gray.300"
-                    children={<CFaLock color="gray.300" />}
-                    tabIndex={-1}
-                    htmlFor="password"
-                  />
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Senha"
-                    tabIndex={2}
-                    title="Senha"
-                    name="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button
-                      h="1.75rem"
-                      size="sm"
-                      tabIndex={3}
-                      onClick={handleShowClick}
-                      title="Exibir a senha"
-                    >
-                      {showPassword ? "Ocultar" : "Exibir"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormHelperText textAlign="right">
-                  <Link tabIndex={4} title="Esqueceu a sua senha?">
-                    Esqueceu a senha?
-                  </Link>
-                </FormHelperText>
-              </FormControl>
-              <Button
-                borderRadius={0}
-                type="submit"
-                variant="solid"
-                colorScheme="teal"
-                width="full"
-                tabIndex={5}
-                onClick={() => onLoginValidation()}
-                backgroundColor={contTryLogin > 3 ? "red.500" : "teal.500"}
-                isDisabled={contTryLogin > 3}
-              >
-                {contTryLogin > 3 ? `Tentativas excedidas` : "Entrar"}
-              </Button>
-            </Stack>
-          )}
-        </Box>
+        {getHeading()}
+        {!isLogged ? getFormBox() : null}
       </Stack>
-      <Box>
-        {!isLogged ? (
-          <>
-            Não possui uma conta?
-            <Link
-              color="teal.500"
-              paddingLeft="10px"
-              href="#"
-              tabIndex={6}
-              title="cadastrar-se"
-            >
-              Cadastrar-se
-            </Link>
-            <Box>
-              <Button
-                h="1.75rem"
-                size="sm"
-                tabIndex={1}
-                onClick={() => setContTryLogin(0)}
-                title="Resetar contador de tentativas"
-                width="2.5rem"
-                padding="0.5rem"
-                fontSize="0.5rem"
-                marginLeft={2}
-                backgroundColor={"teal.500"}
-                name={"Resetar contador de tentativas"}
-              >
-                X
-              </Button>{" "}
-              {`Contador de tentativas: ${contTryLogin}`}
-            </Box>
-            {ListUsers(usersDB)}
-          </>
-        ) : (
-            <Button
-              h="1.75rem"
-              size="sm"
-              tabIndex={1}
-              onClick={() => setIsLogged(false)}
-              title="desconectar-se"
-              width="4.5rem"
-              name="logout"
-            >
-              Sair
-            </Button>
-        )}
-      </Box>
+      <Box>{!isLogged ? getComplementBox() : getUnSignButton()}</Box>
     </Flex>
   );
 }
